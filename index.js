d8b2bd1952e2f91770e5abdce091638268d3a941
student <daisy-carolin@gmail.com>
2021-04-04 18:21:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var person=["Daisi","caroline",26,"kenya"];
+
+var text=" ";
+var x;
+for (x in person){
+    text +=person[x] +" ";
+
+}
+console.log(person);
+
+var house=["furniture","people","utensils","pets"];
+
+var text=" ";
+var h;
+for(h in house){
+    text +=house[h] +" ";
+}
+console.log(house);
+
+let fruits=["lemon","guava","pineaple","mango","kiwi"];
+
+let f="";
+
+for(let f of fruits){
+    text +=f+"<br>";
+}
+console.log(fruits);
+
+let names=["Fajiri","Jamari","Alph","Robin"];
+let n="";
+for(let n of names){
+
+text +=n+"<br>";
+}
+console.log(names);
